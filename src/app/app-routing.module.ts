1b1d1fe10b3366822bb151678a2dd8b62a334f72
15005I <15005@portalcedi.edu.mx>
2025-01-15 21:06:48 -0600

    Prueba

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { RegistroComponent } from './registro/registro.component';
 
 const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'registro',
+    component: RegistroComponent
   },
   {
     path: '',
